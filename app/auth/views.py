bb17ef8d1df02d305597bc4fd3e012aa297d2aa1
JamesKimari <jamesgkimari@gmail.com>
2018-02-04 17:37:46 +0300

    created a login view function for logging in a registered user

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,23 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_user
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .. import db
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+            flash('Invalid username or password')
+
+    title = 'Pitchez Login'
+    return render_template('auth/login.html', login_form = login_form, title = title)
 
 @auth.route('/register', methods = ["GET", "POST"])
 def register():
