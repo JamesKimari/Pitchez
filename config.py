6e17e24a3e73e111ab566729e1d7fad8aed3e586
JamesKimari <jamesgkimari@gmail.com>
2018-02-04 08:51:10 +0300

    defined the app configurations in app.config

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+class Config:
+    """
+    General configuratins parent class
+    """
+    SECRET_KEY = os.environ.get('SECRET KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pasaris:maisiewilliams7@localhost/pitchez'
+    SQLALHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+    """
+    Production configuration child class
+    """
+    pass
+
+class DevConfig(Config):
+    """
+    Development config child class
+    """
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycop2://pasaris:maisiewilliams7@localhost/pitchez'
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
