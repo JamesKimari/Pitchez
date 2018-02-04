c779147d46a1bb5eedbdb6d1fb152260830a4e71
JamesKimari <jamesgkimari@gmail.com>
2018-02-04 14:27:55 +0300

    created a view function for logging in and also registered the auth blueprint in app.init

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,9 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/auth')
     
     return app
 
