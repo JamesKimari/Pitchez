9c2b88e8eed84a42dafc1a74bd6315d694e10f75
JamesKimari <jamesgkimari@gmail.com>
2018-02-04 19:15:12 +0300

    imported logout and login_required attributes in the auth.views module

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, request, flash
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
 from .forms import RegistrationForm, LoginForm
@@ -29,3 +29,9 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html', registration_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
