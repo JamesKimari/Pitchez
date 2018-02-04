b85758980f87db04e043c09089409ed052aa3d2d
JamesKimari <jamesgkimari@gmail.com>
2018-02-04 09:52:53 +0300

    created a view function for the index page

---
+++ app/main/views.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from. import main
+from ..models import User
+from .. import db
+
+@main.route('/')
+def index():
+    """
+    View root page function that returns the index page and its data
+    """
+    title = 'Pitchez'
+
+    return render_template('index.html', title = title)
+
+
