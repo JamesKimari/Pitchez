9314010dced7cfbbc1f014cf372e06b8d1caf322
JamesKimari <jamesgkimari@gmail.com>
2018-02-04 09:26:39 +0300

    registered the main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -15,8 +15,9 @@ def create_app(config_name):
     db.init_app(app)
 
     # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     
-
     return app
 
 
