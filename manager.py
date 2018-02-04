3afa6f1ed44c9688b84b7995f187f546116b3550
JamesKimari <jamesgkimari@gmail.com>
2018-02-04 14:16:43 +0300

    added a decorator and a function for running tests in manager.py

--- manager.py
+++ manager.py
@@ -5,12 +5,19 @@ from flask_migrate import Migrate, MigrateCommand
 
 # creating app instance
 app = create_app('development')
+app = create_app('test')
 
 manager = Manager(app)
 manager.add_command('server', Server)
 
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity = 2).run(tests)
 
 @manager.shell
 def make_shell_context():
