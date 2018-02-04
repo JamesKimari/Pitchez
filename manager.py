59db3d8832775eb129b19a429304240b35d30d77
JamesKimari <jamesgkimari@gmail.com>
2018-02-04 13:54:43 +0300

    initialized database migrations in our database and app using the flask-migrate extension in manager.py

--- manager.py
+++ manager.py
@@ -1,6 +1,7 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User
+from app.models import User, Category, Pitch
+from flask_migrate import Migrate, MigrateCommand
 
 # creating app instance
 app = create_app('development')
@@ -8,9 +9,12 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server', Server)
 
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User)
+    return dict(app = app, db = db, User = User, Category = Category, Pitch = Pitch)
 
 if __name__ == '__main__':
     manager.run()
