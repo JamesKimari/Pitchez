bb6edb9015c8f4b80cd915e5dcff4803b591e01b
JamesKimari <jamesgkimari@gmail.com>
2018-02-05 08:18:47 +0300

    initialized the functionality of uploading pictures in the app

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
@@ -11,12 +12,17 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+photos = UploadSet('photos', IMAGES)
+
 def create_app(config_name):
 
     app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+    # Configure UploadSet
+    configure_uploads(app, photos)
 
     # Initializing Flask Extensions
     db.init_app(app)
