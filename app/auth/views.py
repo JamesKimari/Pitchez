cfe1c1c074fb87766a29ff108db024445fc87d39
JamesKimari <jamesgkimari@gmail.com>
2018-02-05 19:39:27 +0300

    changed the redirect url path for when the login form is submitted

--- app/auth/views.py
+++ app/auth/views.py
@@ -12,7 +12,7 @@ def login():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user, login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            return redirect(url_for('main.new_pitch'))
 
         flash('Invalid username or password')
 
@@ -23,7 +23,7 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data, password_lock = form.password.data)
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
         db.session.add(user)
         db.session.commit()       
         
