bb9c380732886d6b2cb1b46e51994d4ab0b74a33
JamesKimari <jamesgkimari@gmail.com>
2018-02-05 09:05:39 +0300

    initialized the mail class inside the app

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+mail = Mail()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -28,6 +30,7 @@ def create_app(config_name):
     db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY='nexttimemseeh'
+export MAIL_USERNAME='jamesgkimari@gmail.com'
+export MAIL_PASSWORD='reneSSanSipaSariS7'
 
 python manager.py server
