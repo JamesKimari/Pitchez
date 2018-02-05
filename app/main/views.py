7dfb37d1a56756e6c1ca3c08604a5e5f58458e25
JamesKimari <jamesgkimari@gmail.com>
2018-02-05 08:19:23 +0300

    imported the photos functionality in app.views

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from. import main
 from ..models import User, Pitch
 from .forms import PitchForm, UpdateProfile
 from flask_login import login_required, current_user
-from .. import db
+from .. import db, photos
 
 @main.route('/')
 def index():
@@ -41,6 +41,19 @@ def update_profile(uname):
         return redirect(url_for('.profile', uname = user.username))
 
     return render_template('profile/update.html', form = form)
+
+@main.route('/user/<uname>/update/pic', methods = ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+
+    return redirect(url_for('main.profile', uname = uname))
 
 
 
