310dd6749ab969b474ffa73f70ab44b2df91a016
JamesKimari <jamesgkimari@gmail.com>
2018-02-06 08:06:43 +0300

    uninstalled flask-simpleMDE and removed all its configurations

--- config.py
+++ config.py
@@ -7,8 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pasaris:maisiewilliams7@localhost/pitchez'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
-
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'   
     
 class ProdConfig(Config):
     """
