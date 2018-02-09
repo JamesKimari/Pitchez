9a44f41cc84ea201c9511d5f90ca142fdd090343
JamesKimari <jamesgkimari@gmail.com>
2018-02-09 14:18:48 +0300

    third redeploy

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ class Config:
     """
     General configuratins parent class
     """
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pasaris:maisiewilliams7@localhost/pitchez'
+    SECRET_KEY = os.environ.get('SECRET_KEY')    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'   
 
